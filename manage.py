fc22446b92bd27ea71300aae275685f4363d0ec3
Dag Odenhall <dag.odenhall@gmail.com>
2010-10-20 23:19:37 +0200

    More useful manage shell

--- manage.py
+++ manage.py
@@ -17,5 +17,21 @@ manager.add_option('-c', '--config', dest='config',
                    default='stutuz.configs.development')
 
 
+@manager.shell
+def context():
+    from flask import current_app
+    from stutuz import db, models
+    return dict(app=current_app, db=db, models=models)
+
+manager._commands['shell'].banner = \
+"""Interactive stutuz console.
+
+Useful names:
+
+  app     Flask application for stutuz
+  db      The database instance
+  models  Namespace for all models
+"""
+
 if __name__ == '__main__':
     manager.run()
