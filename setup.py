55f9c324f0478a1402fa4212fb896bf760d3abbb
Dag Odenhall <dag.odenhall@gmail.com>
2010-11-27 22:38:08 +0100

    Finish port of tests to Attest

--- setup.py
+++ setup.py
@@ -49,12 +49,11 @@ setup(
 
     tests_require=[
         'Attest>=0.3',
-        'Flask-Testing>=0.2.4',
-        'unittest2>=0.5.1',
         'lxml>=2.2.8'
     ],
 
-    test_suite='stutuz.tests',
+    test_loader='attest:Loader',
+    test_suite=b'stutuz.tests.all',
 
     classifiers=[
         'Development Status :: 1 - Planning',
--- stutuz/tests/__init__.py
+++ stutuz/tests/__init__.py
@@ -6,15 +6,7 @@ from __future__ import with_statement
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import re
-from functools import partial
-
 from attest import Tests
-from flaskext.testing import TestCase
-import unittest2
-from lxml import etree
-
-from stutuz import create_app
 
 
 suite = lambda mod: 'stutuz.tests.' + mod + '.suite'
@@ -25,28 +17,3 @@ all = Tests([suite('converters'),
              suite('zodb'),
              suite('export'),
             ])
-
-
-class TestBase(TestCase, unittest2.TestCase):
-
-    def setUp(self):
-        self.app.preprocess_request()
-        if hasattr(self, 'setup'):
-            self.setup()
-
-    def tearDown(self):
-        self.app.process_response(self.app.response_class())
-        if hasattr(self, 'teardown'):
-            self.teardown()
-
-    def create_app(self):
-        return create_app('stutuz.configs.testing')
-
-    def __getattr__(self, name):
-        name = re.sub(r'_([a-z])', lambda m: m.group(1).upper(), name)
-        return partial(getattr(unittest2.TestCase, name), self)
-
-    def assert_xml(self, response, xpath):
-        """An XPath query on a response returns a True-like result."""
-        doc = etree.fromstringlist(response.data)
-        self.assert_(doc.xpath(xpath))
