827fc42b9f8262aa69df6ca9e53fb7ed010c7df3
Dag Odenhall <dag.odenhall@gmail.com>
2010-12-07 21:41:35 +0100

    Begin merging HTML and JSON routes

--- stutuz/modules/relvlast/__init__.py
+++ stutuz/modules/relvlast/__init__.py
@@ -6,10 +6,16 @@ from __future__ import with_statement
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from flask import Module
+from flask import Module, request, jsonify
 from flaskext.genshi import render_response
 
 from stutuz.extensions import db
+
+
+def wants_json():
+    return request.is_xhr or \
+           request.accept_mimetypes.best == 'application/json' or \
+           'json' in request.args
 
 
 mod = Module(__name__)
@@ -17,10 +23,17 @@ mod = Module(__name__)
 
 @mod.route('/<entry:entry>/')
 def entry(entry):
-    return render_response('relvlast/entry.html', {
-        'entry': db['entries'][entry],
-        'languages': db['languages'],
-    })
+    entry = db['entries'][entry]
+
+    if wants_json():
+        data = dict(id=entry.id, type=entry.type, translations={})
+        if hasattr(entry, 'affixes'):
+            data['affixes'] = map(str, entry.affixes)
+        for code, history in entry.translations.iteritems():
+            data['translations'][code] = str(history.newest.object.definition)
+        return jsonify(data)
+
+    return render_response('relvlast/entry.html', {'entry': entry})
 
 
 @mod.route('/<entry:entry>/revisions/<lang:language>/')
